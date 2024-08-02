e99b6f0703712ac95894f7579bc1b7a5f148f276
pierrepas <pierrepas@yahoo.fr>
2024-08-02 22:23:41 +0200

    Improvement of performance using goroutines

--- cmd/go_manage_my_files/main.go
+++ cmd/go_manage_my_files/main.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"go_manage_my_files/pkg/check_duplicate_files"
+	"log"
+	"time"
 )
 
 func main() {
-	var actionFlag = flag.String("action", "check_duplicates", "What action the program should take")
-	var outputFileFlag = flag.String("output", "duplicates_found.txt", "What file the duplicates should be listed into")
-	var pathtoSearchFlag = flag.String("path", ".", "What path the duplicates should be searched from")
+	startTime := time.Now()
+	log.SetFlags(0) // Removes time stamp from log
+	actionFlag := flag.String("action", "check_duplicates", "What action the program should take")
+	outputFileFlag := flag.String("output", "duplicates_found.txt", "What file the duplicates should be listed into")
+	pathtoSearchFlag := flag.String("path", ".", "What path the duplicates should be searched from")
 	flag.Parse()
 
 	if *actionFlag == "check_duplicates" {
@@ -17,4 +21,8 @@ func main() {
 	} else {
 		fmt.Println("Flag not recognised")
 	}
+	timeSinceStart := time.Since(startTime)
+	log.Println()
+	log.Printf("Script took %s", timeSinceStart)
+	log.Println()
 }
--- pkg/check_duplicate_files/check_duplicate_files.go
+++ pkg/check_duplicate_files/check_duplicate_files.go
@@ -8,58 +8,102 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sync"
 )
 
-func CheckForDuplicates(outputFile string, pathToSearch string) {
-	fmt.Println("Searching the path: " + pathToSearch)
-	fmt.Println("Writing to file: " + outputFile)
-	fmt.Println()
-	f, err := os.OpenFile(outputFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+type fileInfo struct {
+	path string
+	hash string
+}
 
+func CheckForDuplicates(outputFile string, pathToSearch string) {
+	log.Println("Searching the path:", pathToSearch)
+	log.Println("Writing to file:", outputFile)
+	log.Println()
+	duplicateCount := 0
+
+	f, err := os.OpenFile(outputFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
 
-	fileHashes := make(map[string][]string)
+	filesChan := make(chan string)
+	resultsChan := make(chan fileInfo)
+	var wg sync.WaitGroup
 
-	err = filepath.Walk(pathToSearch, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			hash, err := calculateMD5(path)
+	// Start worker goroutines
+	numWorkers := runtime.NumCPU()
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go worker(filesChan, resultsChan, &wg)
+	}
+
+	// Start a goroutine to walk the directory
+	go func() {
+		err := filepath.Walk(pathToSearch, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-			fileHashes[hash] = append(fileHashes[hash], path)
+			if !info.IsDir() {
+				filesChan <- path
+			}
+			return nil
+		})
+		if err != nil {
+			log.Printf("Error walking through directory: %v\n", err)
 		}
-		return nil
-	})
+		close(filesChan)
+	}()
 
-	if err != nil {
-		fmt.Printf("Error walking through directory: %v\n", err)
-		return
+	// Use a separate goroutine to collect results
+	go func() {
+		wg.Wait()
+		close(resultsChan)
+	}()
+
+	// Collect results in the main goroutine
+	fileHashes := make(map[string][]string)
+	for result := range resultsChan {
+		fileHashes[result.hash] = append(fileHashes[result.hash], result.path)
 	}
 
+	// Write results
 	writer := bufio.NewWriter(f)
 	duplicatesFound := false
-	for _, paths := range fileHashes {
+	for hash, paths := range fileHashes {
 		if len(paths) > 1 {
 			duplicatesFound = true
-			fmt.Println("Duplicate files found:")
+			log.Println("Duplicate files found (hash:", hash, "):")
+			duplicateCount++
 			for _, path := range paths {
-				fmt.Println(path)
+				log.Println(path)
 				writer.WriteString(path + "\n")
 			}
-			fmt.Println()
+			log.Println()
 			writer.WriteString("\n")
 		}
 	}
 
 	writer.Flush()
+	dupCntStr := fmt.Sprint(duplicateCount)
+	log.Println(dupCntStr + " duplicates found.")
 
 	if !duplicatesFound {
-		fmt.Println("No duplicate files found.")
+		log.Println("No duplicate files found.")
+	}
+}
+
+func worker(files <-chan string, results chan<- fileInfo, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for file := range files {
+		hash, err := calculateMD5(file)
+		if err != nil {
+			log.Printf("Error calculating hash for %s: %v\n", file, err)
+			continue
+		}
+		results <- fileInfo{path: file, hash: hash}
 	}
 }
 
